d02c6243d1eba45d5181b0e0883fbd195d3d170d
Vladyslav <mrllltrixisworse@gmail.com>
2018-09-01 14:06:15 +0300

    Add toggle button.

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,5 +1,6 @@
 function subscribe() {
     var email = $('#email').val();
+    $("#modal-container-876760 .close").click();
     $.ajax({
         url: "php/insert.php",
         type: "post",
@@ -9,8 +10,21 @@ function subscribe() {
             console.log(dataParsed);
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert("Status: " + textStatus);
-            alert("Error: " + errorThrown);
+            console.log(textStatus);
+            console.log(errorThrown);
         }
     });
 }
+$(function() {
+    $('#toggle-event').change(function() {
+        if($(this).prop('checked')) {
+            $('#off-light1').attr('src','./img/chips/ff2.jpg');
+            $('#off-light2').attr('src','./img/chips/kk2.jpg');
+            $('#off-light3').attr('src','./img/chips/Edited2.jpg');
+        } else {
+            $('#off-light1').attr('src','./img/chips/ff.jpg');
+            $('#off-light2').attr('src','./img/chips/kk1.jpg');
+            $('#off-light3').attr('src','./img/chips/Edited1.jpg');
+        }
+    })
+})
